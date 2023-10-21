4a6d3d10f10474d8a73d4423445312e9b7a202a4
Jesse <jeisey91@gmail.com>
2023-10-21 16:14:59 +0000

    Updated instructions

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def main():
     2. **Fill out the required fields** based on your league selection:
     - **ESPN**:
         - *League ID*: [Find it here](https://support.espn.com/hc/en-us/articles/360045432432-League-ID).
-        - *SWID and ESPN_S2*: Use this [Chrome extension](https://chrome.google.com/webstore/detail/espn-private-league-key-a/bakealnpgdijapoiibbgdbogehhmaopn) or follow [manual steps](link_to_manual_steps).
+        - *SWID and ESPN_S2*: Use this [Chrome extension](https://chrome.google.com/webstore/detail/espn-private-league-key-a/bakealnpgdijapoiibbgdbogehhmaopn) or follow [manual steps](https://www.gamedaybot.com/help/espn_s2-and-swid/).
     - **Yahoo**:
         - *League ID*: Navigate to Yahoo Fantasy Sports → Click your league → Mouse over **League**, click **Settings**. The League ID number is listed first.
     3. **Hit "🤖Generate AI Summary"** to get your weekly summary.
