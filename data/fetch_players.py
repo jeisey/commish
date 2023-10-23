c1c9c2f5700832726cd9599e37bdfc0ed3490954
Jesse <jeisey91@gmail.com>
2023-10-23 22:45:42 +0000

    Added github workflow action for Sleeper data

---
+++ data/fetch_players.py
@@ -0,0 +1,13 @@
+# fetch_players.py
+from sleeper_wrapper import Players
+import json
+
+def save_players_data():
+    players = Players()
+    all_players = players.get_all_players(sport="nfl")
+    
+    with open('players_data.json', 'w') as f:
+        json.dump(all_players, f)
+
+if __name__ == "__main__":
+    save_players_data()
