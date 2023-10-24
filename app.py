0510cdba63e95a8b67c47d7434c2a1017dc3f6e4
Jesse <jeisey91@gmail.com>
2023-10-24 04:30:02 +0000

    Updated progress loader

--- app.py
+++ app.py
@@ -63,73 +63,84 @@ def main():
         # Handling form 
         if submit_button:
             try:
-                # with st.spinner('Generating your summary... This will take about 15 seconds.'):
-                with st.status("Generating your summary... This will take about 15 seconds", expanded=True) as status:
-                    if league_type == "ESPN":
-                        required_fields = ['LeagueID', 'SWID', 'ESPN2_Id', 'Character Description', 'Trash Talk Level']
-                    else:
-                        required_fields = ['LeagueID', 'Character Description', 'Trash Talk Level']
-                    # Input validation
-                    status.text('Validating credentials...')
-                    for field in required_fields:
-                        value = st.session_state.get(field, None)
-                        if not value:
-                            st.error(f"{field} is required.")
-                            return  # Stop execution if any required field is empty
-                    if all(st.session_state.get(field, None) for field in required_fields):
-                        league_id = st.session_state.get('LeagueID', 'Not provided')
-                        character_description = st.session_state.get('Character Description', 'Not provided')
-                        trash_talk_level = st.session_state.get('Trash Talk Level', 'Not provided')
-                        swid = st.session_state.get('SWID', 'Not provided')
-                        espn2 = st.session_state.get('ESPN2_Id', 'Not provided')
+                progress = st.progress(0)
+                progress.text('Starting...')
+                
+                required_fields = ['LeagueID', 'Character Description', 'Trash Talk Level']
+                if league_type == "ESPN":
+                    required_fields.extend(['SWID', 'ESPN2_Id'])
+                
+                # Input validation
+                progress.text('Validating credentials...')
+                progress.progress(5)
+                for field in required_fields:
+                    value = st.session_state.get(field, None)
+                    if not value:
+                        st.error(f"{field} is required.")
+                        return  # Stop execution if any required field is empty
+                
+                league_id = st.session_state.get('LeagueID', 'Not provided')
+                character_description = st.session_state.get('Character Description', 'Not provided')
+                trash_talk_level = st.session_state.get('Trash Talk Level', 'Not provided')
+                swid = st.session_state.get('SWID', 'Not provided')
+                espn2 = st.session_state.get('ESPN2_Id', 'Not provided')
+                
+                # Fetch open ai key
+                openai_api_key = st.secrets["openai_api_key"]
+                openai.api_key = openai_api_key
 
-                        # Fetch open ai key
-                        openai_api_key=st.secrets["openai_api_key"]
-                        openai.api_key=openai_api_key
-
-                        # Moderate the character description
-                        status.text('Validating character...')
-                        if not summary_generator.moderate_text(character_description):
-                            st.error("The character description contains inappropriate content. Please try again.")
-                            return  # Stop execution if moderation fails
-                        
-                        status.text('Fetching league summary...')
-                        if league_type == "ESPN":
-                            summary, debug_info = summary_generator.get_espn_league_summary(
-                                league_id, espn2, swid 
-                            )
-                        elif league_type == "Yahoo":
-                            auth_directory = "auth"
-                            summary = summary_generator.get_yahoo_league_summary(
-                                league_id, auth_directory
-                            )
-                        elif league_type == "Sleeper":
-                            auth_directory = "auth"
-                            summary = summary_generator.generate_sleeper_summary(
-                                league_id, 7 #replace with func inside for current week
-                            )
-                        
-                        status.text('Generating AI summary...')
-                        gpt4_summary_stream = summary_generator.generate_gpt4_summary_streaming(
-                            summary, character_description, trash_talk_level
-                        )
-                        
-                        with st.chat_message("Commish", avatar="🤖"):
-                            message_placeholder = st.empty()
-                            full_response = ""
-                            for chunk in gpt4_summary_stream:
-                                full_response += chunk
-                                message_placeholder.markdown(full_response + "▌")
-                            message_placeholder.markdown(full_response)
-                            
-                            # Display the full response within a code block which provides a copy button
-                            st.markdown("**Click the copy icon** 📋 below in top right corner to copy your summary and paste it wherever you see fit!")
-                            st.code(full_response, language="")
-                            st.markdown("Don't like this one? Try entering a **new character** and it will **start generating immediately**.")
+                # Moderate the character description
+                progress.text('Validating character...')
+                progress.progress(15)
+                if not summary_generator.moderate_text(character_description):
+                    st.error("The character description contains inappropriate content. Please try again.")
+                    return  # Stop execution if moderation fails
+                
+                # Fetching league summary
+                progress.text('Fetching league summary...')
+                progress.progress(30)
+                if league_type == "ESPN":
+                    summary, debug_info = summary_generator.get_espn_league_summary(
+                        league_id, espn2, swid 
+                    )
+                elif league_type == "Yahoo":
+                    auth_directory = "auth"
+                    summary = summary_generator.get_yahoo_league_summary(
+                        league_id, auth_directory
+                    )
+                elif league_type == "Sleeper":
+                    auth_directory = "auth"
+                    summary = summary_generator.generate_sleeper_summary(
+                        league_id
+                    )
+                
+                progress.text('Generating AI summary...')
+                progress.progress(50)
+                gpt4_summary_stream = summary_generator.generate_gpt4_summary_streaming(
+                    summary, character_description, trash_talk_level
+                )
+                
+                with st.chat_message("Commish", avatar="🤖"):
+                    message_placeholder = st.empty()
+                    full_response = ""
+                    progress.progress(70)
+                    for chunk in gpt4_summary_stream:
+                        full_response += chunk
+                        message_placeholder.markdown(full_response + "▌")
+                    message_placeholder.markdown(full_response)
+                    
+                    # Display the full response within a code block which provides a copy button
+                    st.markdown("**Click the copy icon** 📋 below in top right corner to copy your summary and paste it wherever you see fit!")
+                    st.code(full_response, language="")
+                    st.markdown("Don't like this one? Try entering a **new character** and it will **start generating immediately**.")
+                    
+                progress.text('Done!')
+                progress.progress(100)
+                
             except Exception as e:
-                status.error(f"An error occurred: {str(e)}")
                 st.error(f"An error occurred: {str(e)}")
                 LOGGER.exception(e)
+
 if __name__ == "__main__":
     main()
 
